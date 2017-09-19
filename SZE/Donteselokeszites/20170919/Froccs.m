abc3cbea25c4e5dbd16c422d6f0570ff51f8e7a1
Mate Hegyhati <hegyhati.work@gmail.com>
2017-09-19 15:58:59 +0200

    Froccs-plan

--- SZE/Donteselokeszites/20170919/Froccs.m
+++ SZE/Donteselokeszites/20170919/Froccs.m
@@ -1,5 +1,9 @@
 #valtozok
+# minden froccstipushoz kell egy folytonos valtozo
 
 #korlatozasok
+# nem hasznalhatunk tobb bort, szodat, mint amennyi van.
+
 
 #celfuggveny
+# profit legyen minel nagyobb
