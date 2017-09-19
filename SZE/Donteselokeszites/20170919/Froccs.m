ec3871de2c80cf64b3862e12a3f5470d4fa8c752
Mate Hegyhati <hegyhati.work@gmail.com>
2017-09-19 16:10:16 +0200

    Froccs-constraints

--- SZE/Donteselokeszites/20170919/Froccs.m
+++ SZE/Donteselokeszites/20170919/Froccs.m
@@ -1,16 +1,24 @@
 #valtozok
-var xKF;
-var xNF;
-var xHL;
-var xHM;
-var xVHM;
-var xKrF;
-var xPF;
+var xKF; # adag
+var xNF; # adag
+var xHL; # adag
+var xHM; # adag
+var xVHM; # adag
+var xKrF; # adag
+var xSF; # adag
+var xPF; # adag
 
 
 #korlatozasok
-# nem hasznalhatunk tobb bort, szodat, mint amennyi van.
+
+s.t. Bor: # deciliter
+  1*xKF + 2*xNF + 1*xHL + 3*xHM + 2*xVHM + 9*xKrF + 1*xSF + 6*xPF <= 1000;
+
+s.t. Szoda: # deciliter
+  1*xKF + 1*xNF + 2*xHL + 2*xHM + 3*xVHM + 1*xKrF + 9*xSF + 3*xPF <= 1500;
 
 
 #celfuggveny
 # profit legyen minel nagyobb
+
+maximize Profit: 0;
