ecb0cdc800b9b97275f7923eae3bab122b37da77
Mate Hegyhati <hegyhati.work@gmail.com>
2017-09-19 16:00:52 +0200

    Froccs-variables

--- SZE/Donteselokeszites/20170919/Froccs.m
+++ SZE/Donteselokeszites/20170919/Froccs.m
@@ -1,5 +1,12 @@
 #valtozok
-# minden froccstipushoz kell egy folytonos valtozo
+var xKF;
+var xNF;
+var xHL;
+var xHM;
+var xVHM;
+var xKrF;
+var xPF;
+
 
 #korlatozasok
 # nem hasznalhatunk tobb bort, szodat, mint amennyi van.
