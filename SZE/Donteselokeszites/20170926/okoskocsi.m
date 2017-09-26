e72116f33d3199cf935af2a5f6679f74ee257046
Mate Hegyhati <hegyhati.work@gmail.com>
2017-09-26 16:35:20 +0200

    Csillivilli kimenet

--- SZE/Donteselokeszites/20170926/okoskocsi.m
+++ SZE/Donteselokeszites/20170926/okoskocsi.m
@@ -19,3 +19,13 @@ s.t. MindenKocsibolEgyetSzeretnenk {k in Kocsik}:
 # Objective
 minimize Koltseg:
     sum{k in Kocsik, n in Napok} megvesz[k,n] * ar[k,n];
+
+solve;
+
+for{k in Kocsik, n in Napok : megvesz[k,n]=1}
+{
+    printf "A %s-t %s napon vesszuk meg %d Ft-ert.\n",k,n,ar[k,n];
+}
+
+
+end;
