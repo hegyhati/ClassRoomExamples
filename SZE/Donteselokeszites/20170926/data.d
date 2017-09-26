096645b47ac26fb57365e5486fb2506ff6ece9a5
Mate Hegyhati <hegyhati.work@gmail.com>
2017-09-26 16:44:54 +0200

    .gitignore-bol .d fajlok kivetele. kocsi.d

---
+++ SZE/Donteselokeszites/20170926/data.d
@@ -0,0 +1,10 @@
+set Kocsik := Wartburg Lada Fiat Trabant Skoda;
+set Napok := Hetfo Kedd Szerda Csutortok Pentek Szombat;
+
+param ar:    Hetfo Kedd  Szerda Csutortok Pentek Szombat:=
+    Wartburg 60000 65000 61000  66000     60000  1
+    Lada     50000 55000 63000  60000     52000  1
+    Fiat     30000 32000 33000  30000     27000  1
+    Trabant  70000 65000 77000  85000     100000 1
+    Skoda    65000 70000 75000  90000     70000  1
+    ;
