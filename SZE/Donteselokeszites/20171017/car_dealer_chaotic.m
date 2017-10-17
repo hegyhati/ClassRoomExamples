1c299a3414830b816abf5f7efc2948e54f8f8b00
Mate Hegyhati <hegyhati.work@gmail.com>
2017-10-17 14:28:51 +0200

    Nice output

--- SZE/Donteselokeszites/20171017/car_dealer_chaotic.m
+++ SZE/Donteselokeszites/20171017/car_dealer_chaotic.m
@@ -45,3 +45,24 @@ s.t. RedundantConstraint2 {c in Cars}:
 ## I want to maximize my budget in the end
 maximize FinalBudget: startbudget + sum {d in Days,c in Cars} price[d,c]*(sell[d,c]-buy[d,c]);
 
+
+solve;
+
+for {d in Days}
+{
+    printf "Day %2d:\n",d;
+    printf "\tBUY:";
+    for{c in Cars : buy[d,c]!=0}
+        printf " %s(%d)",c,buy[d,c];
+    printf "\n";
+    printf "\tSELL:";
+    for{c in Cars : sell[d,c]!=0}
+        printf " %s(%d)",c,sell[d,c];
+    printf "\n\t--------------------\n";
+    printf "\tGARAGE [%d/%d]:", sum{dd in 1..d, c in Cars} (buy[dd,c]-sell[dd,c]),garagecapacity;
+    for{c in Cars : sum{dd in 1..d} (buy[dd,c]-sell[dd,c])!= 0}
+       printf " %s(%d)",c,sum{dd in 1..d} (buy[dd,c]-sell[dd,c]);
+    printf "\n";
+    printf "\tBUDGET: %d", startbudget+sum{dd in 1..d, c in Cars} price[dd,c]*(sell[dd,c]-buy[dd,c]);
+    printf "\n\n";    
+}
