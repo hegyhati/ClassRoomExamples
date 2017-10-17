eb4c5223f971e0f4666315ffe6403cde5e951d5c
Mate Hegyhati <hegyhati.work@gmail.com>
2017-10-17 13:39:18 +0200

    car data

---
+++ SZE/Donteselokeszites/20171017/car_data.d
@@ -0,0 +1,18 @@
+param startbudget := 200000;
+
+param garagecapacity :=  4;
+
+param nDays := 7;
+
+set Cars := Trabant Skoda Wartburg Fiat;
+
+param price :
+        Trabant Skoda   Wartburg    Fiat :=
+    1   120000  110000  100000      95000
+    2   135000  120000  111000      100000
+    3   125000  115000  120000      110000
+    4   120000  111000  95000       105000
+    5   111000  111000  111000      111000
+    6   120000  110000  101000      100000
+    7   123000  123000  123000      123000
+    ;
