76e7f4ad5767a10368424f6875184c15590fbde9
Mate Hegyhati <hegyhati.work@gmail.com>
2017-11-16 01:48:09 +0200

    COCC C spoilers

---
+++ COCC/Programming/20171115/first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    x=1;
+
+    
+    while(x<=200)
+    {
+        printf("%4d\n",x);
+        x=x+29;
+    }
+
+
+    return 0;
+}
---
+++ COCC/Programming/20171115/last.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x;
+    int a, b;
+    int facta,factb,factamb;
+    int row, column;
+    
+    scanf("%d",&n);
+
+    row=0;
+    while(row<=n+2)
+    {
+        column=0;
+        while(column<=n+1-row)
+        {
+            a=row+column;
+            b=row;
+            
+            x=1; facta=1;
+            while(x<=a)
+            {
+                facta=facta*x;
+                x=x+1;
+            }
+            x=1; factb=1;
+            while(x<=b)
+            {
+                factb=factb*x;
+                x=x+1;
+            }
+            x=1; factamb=1;
+            while(x<=a-b)
+            {
+                factamb=factamb*x;
+                x=x+1;
+            }
+            printf("%4d",facta/factb/factamb);
+            column = column+1;            
+        }
+        printf("\n");
+        row=row+1;
+    }    
+   
+    return 0;
+}
