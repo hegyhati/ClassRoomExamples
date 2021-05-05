e07609bd3df0dbfcfd77666fa28003cbae60f0b9
hegyhati <hegyhati.work@gmail.com>
2021-05-05 20:07:22 +0200

    Bit of polishing

--- SOE/ProgAlap2/20210505_Gyakorlas/calendar_ui.py
+++ SOE/ProgAlap2/20210505_Gyakorlas/calendar_ui.py
@@ -1,68 +1,54 @@
 from calendar_logic import Event, Calendar
 import tkinter as tk
-from tkinter.ttk import Combobox
+from tkinter.ttk import Combobox,Separator
 
+_dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 class WeekView (tk.Frame):
-    __dayNames = ["Monday", "Tuesday", "Wednesday",
-                  "Thursday", "Friday", "Saturday", "Sunday"]
 
-    def __init__(self, parent, calendar):
-        super().__init__(parent)
-        for idx, day in enumerate(self.__dayNames):
-            tk.Label(self, text=day).grid(row=0, column=idx+1)
+    def __init__(self, master, calendar):
+        super().__init__(master)
+        for idx, day in enumerate(_dayNames):
+            tk.Label(self, text=day, width=15).grid(row=0, column=idx+1)
 
         for start in range(24):
             tk.Label(
                 self, text=f"{start}-{start+1}").grid(row=start+1, column=0)
 
-        self.__calendar = calendar
         for event in calendar._events:
-            self.new_event(event)
+            self.add_event(event)
 
-    def new_event(self, event):
-        tk.Label(self, text=event.name, borderwidth=3, relief="ridge").grid(
-            column=event.day+1, row=event.start+1, sticky="NSEW", rowspan=event.end-event.start)
+    def add_event(self, event):
+        tk.Label(self, text=event.name, borderwidth=3, relief="ridge").grid(column=event.day+1, row=event.start+1, sticky="NEWS", rowspan=event.end-event.start)
 
 
 class NewEventFrame(tk.Frame):
-    __dayNames = ["Monday", "Tuesday", "Wednesday",
-                  "Thursday", "Friday", "Saturday", "Sunday"]
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        widgets = []
-        widgets.append(tk.Label(self, text="Name:"))
+    def __init__(self, master):
+        super().__init__(master)
         self.nameVar = tk.StringVar(self, value="")
-        widgets.append(tk.Entry(self, textvariable=self.nameVar))
-        widgets.append(tk.Label(self, text="Day:"))
-        self.dayVar = tk.StringVar(self, value=self.__dayNames[0])
-        widgets.append(
-            Combobox(self, textvariable=self.dayVar, values=self.__dayNames))
-        widgets.append(tk.Label(self, text="Start:"))
+        self.dayVar = tk.StringVar(self, value=_dayNames[0])
         self.startVar = tk.IntVar(self, value=8)
-        widgets.append(tk.Spinbox(
-            self, textvariable=self.startVar, from_=0, to=23))
-        widgets.append(tk.Label(self, text="End:"))
         self.endVar = tk.IntVar(self, value=9)
-        widgets.append(tk.Spinbox(
-            self, textvariable=self.endVar, from_=1, to=24))
-        widgets.append(
-            tk.Button(self, text="Add Event", command=self.new_event))
-        self.messageVar = tk.StringVar(self, value="")
-        widgets.append(tk.Label(self, textvariable=self.messageVar))
-
+        widgets = [
+            tk.Label(self, text="Name:"),
+            tk.Entry(self, textvariable=self.nameVar),
+            tk.Label(self, text="Day:"),
+            Combobox(self, textvariable=self.dayVar, values=_dayNames),
+            tk.Label(self, text="Start:"),
+            tk.Spinbox(self, textvariable=self.startVar, from_=0, to=23),
+            tk.Label(self, text="End:"),
+            tk.Spinbox(self, textvariable=self.endVar, from_=1, to=24),
+            tk.Button(self, text="Add Event", command=self.new_event)
+        ]
         for widget in widgets:
             widget.pack(side="top", fill="x")
 
     def new_event(self):
         try:
-            newEvent = Event(self.nameVar.get(), self.__dayNames.index(
-                self.dayVar.get()), self.startVar.get(), self.endVar.get())
-            self.messageVar.set("Event successfully created.")
-            self.master.new_event(newEvent)            
+            self.master.new_event(Event(self.nameVar.get(), _dayNames.index(
+                self.dayVar.get()), self.startVar.get(), self.endVar.get()))
         except Exception as e:
-            self.messageVar.set(f"Cannot create new event: {e}")
+            self.master.display_message(f"Cannot create new event: {e}")
 
 
 class CalendarManager(tk.Tk):
@@ -70,12 +56,16 @@ class CalendarManager(tk.Tk):
     def save_to_pickle(self):
         self._calendar.save_to_pickle(self._filename)
 
+    def display_message(self,message:str):
+        self.messageVar.set(message)
+
     def new_event(self, event):
         try:
             self._calendar.add_event(event)
-            self.weekView.new_event(event)
+            self.weekView.add_event(event)
+            self.display_message("Event successfully created and added to calendar")
         except Exception as e:
-            self.newEvent.messageVar.set(f"Cannot add new event: {e}")
+            self.display_message(f"Cannot add new event: {e}")
         
 
     def __init__(self, filename: str = "weekly_schedule.pickle"):
@@ -83,12 +73,12 @@ class CalendarManager(tk.Tk):
         self._filename = filename
         self._calendar = Calendar()
         self._calendar.load_from_pickle(filename)
-
-        tk.Button(self, text=f"SAVE Calendar to {self._filename}.", command=self.save_to_pickle).pack(
-            side="top", fill="x")
-
+        self.messageVar=tk.StringVar(self,value="")
         self.newEvent = NewEventFrame(self)
-        self.newEvent.pack(side="left")
-
         self.weekView = WeekView(self, self._calendar)
-        self.weekView.pack(side="left")
+
+        tk.Button(self, text=f"SAVE Calendar to {self._filename}.", command=self.save_to_pickle).grid(row=0,column=0,columnspan=3,sticky="NEWS")    
+        self.newEvent.grid(row=1, column=0,sticky="NEWS")
+        Separator(self,orient="vertical").grid(row=1,column=1,sticky="ns")
+        self.weekView.grid(row=1,column=2, sticky="NEWS")
+        tk.Label(self,textvariable=self.messageVar,relief="sunken").grid(row=2,column=0,columnspan=3, sticky="NEWS")
